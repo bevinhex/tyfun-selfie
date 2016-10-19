9bcacbdfe80770233c6198c5227b1239e489f325
bevin <bevinhex@yahoo.com>
2016-10-19 13:11:38 +0200

    basic pages

---
+++ client/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+import {render} from 'react-dom';
+import {Router,Route,Link,browserHistory,IndexRoute} from 'react-router';
+
+import App from './component/App.jsx';
+import LoginPage from './component/LoginPage.jsx';
+
+Meteor.startup(()=>{
+	render((
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+				<IndexRoute component={LoginPage}/>
+				<Route path="game" component={LoginPage}/>
+			</Route>
+		</Router>
+	),document.getElementById('render-target'));
+});
